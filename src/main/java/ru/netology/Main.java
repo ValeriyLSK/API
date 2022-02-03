f1bee520fc10bf80dd1c84284c2cf367dbef4d62
ValeriyLSK <valeriyleschenko@yandex.ru>
2022-02-03 18:52:54 +0300

    initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 444444";
+        post.patronymic = "Иванович";
+        post.phone = "+79999999999";
+        post.surname = "Иванов";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = "01";
+        post.birthday.month = "01";
+        post.birthday.year = "1990";
+    }
+}
